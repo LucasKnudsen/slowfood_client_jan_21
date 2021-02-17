d448ad7c2cd4aacffc2283e32ceb0e8ac7c199da
Lucas Knudsen <laksteelhouse@gmail.com>
2021-02-17 21:35:46 +0100

    added renderForm functionality that will remove form if user is authenticated.

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -4,7 +4,11 @@ import axios from 'axios'
 
 
 class Registration extends Component {
-  
+  state = {
+    renderForm: true
+  }
+
+
   registerUser = async (event) => {
     event.preventDefault()
 
@@ -22,38 +26,52 @@ class Registration extends Component {
       token_type: 'Bearer'
     }
     localStorage.setItem('credentials', JSON.stringify(userCredentials));
-    this.props.setAuthentication()
 
+    if (response.data.data.email) {
+      this.props.setAuthentication()
+      this.setState({ renderForm: false })
+      this.setState({ greeting: `Bon Appétit ${response.data.data.email}!` })
+    } else {
+      this.setState({ errorMessage: response })
+    }
   }
-  
+
   render() {
     return (
-      <Form onSubmit={(event) => this.registerUser(event)} data-cy='registration-form'>
-        <Form.Group>
-          <Form.Field
-            data-cy='email-field'
-            name="email"
-            control={Input}
-            label='email'
-            placeholder='email'
-          />
-          <Form.Field
-            data-cy='password-field'
-            name="password"
-            control={Input}
-            label='password'
-            placeholder='password'
-          />
-          <Form.Field
-            data-cy='password-confirmation-field'
-            name="password_confirmation"
-            control={Input}
-            label='confirm password'
-            placeholder='retype password'
-          />
-          <Button data-cy='submit' type='submit'>Register!</Button>
-        </Form.Group>
-      </Form>
+      <div>
+        {this.state.renderForm ? (
+          <Form onSubmit={(event) => this.registerUser(event)} data-cy='registration-form'>
+            <Form.Group>
+              <Form.Field
+                data-cy='email-field'
+                name="email"
+                control={Input}
+                label='email'
+                placeholder='email'
+              />
+              <Form.Field
+                data-cy='password-field'
+                name="password"
+                control={Input}
+                label='password'
+                placeholder='password'
+              />
+              <Form.Field
+                data-cy='password-confirmation-field'
+                name="password_confirmation"
+                control={Input}
+                label='confirm password'
+                placeholder='retype password'
+              />
+              <Button data-cy='submit' type='submit'>Register!</Button>
+            </Form.Group>
+            {this.state.errorMessage && <p>{this.state.errorMessage}</p>}
+          </Form>
+        ) : (
+            <h4>{this.state.greeting}</h4>
+          ) 
+        }
+      </div>
     )
   }
 }
