6a0a1894da5f99aa168f0b86cbe827b901aedddb
Lucas Knudsen <laksteelhouse@gmail.com>
2021-02-21 16:24:55 +0100

    Refactors a bit and adds functionality to the confirm order button

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import MenuList from '../components/MenuList'
-import { Header, Grid, Item, Icon, Container } from 'semantic-ui-react'
+import { Header, Grid, Item, Icon, Container, Button } from 'semantic-ui-react'
 import Registration from '../components/Registration';
 import axios from 'axios';
 import ItemList from '../components/ItemList'
@@ -15,27 +15,26 @@ class Menu extends React.Component {
 
   addToOrder = async (product) => {
     let authHeader = JSON.parse(localStorage.getItem("credentials"))
-    if (!this.state.currentOrder){
+    if (!this.state.currentOrder) {
       try {
         let response = await axios.post(
           "/orders",
           { product_id: product.id },
           { headers: authHeader })
-          this.setState({
-            currentOrder: response.data.order,
-            orderMessage: `${product.title} was added to your order`
-          })
-        
+        this.setState({
+          orderMessage: `${product.title} was added to your order`,
+          currentOrder: response.data.order
+        })
       }
       catch (error) {
         this.setState({ orderMessage: "Product couldn't be added to your order. Try again later" })
       }
     } else {
       try {
-      let response = await axios.put(
-        `/orders/${this.state.currentOrder.id}`,
-        { product_id: product.id, order_id: this.state.currentOrder.id },
-        { headers: authHeader })
+        let response = await axios.put(
+          `/orders/${this.state.currentOrder.id}`,
+          { product_id: product.id, order_id: this.state.currentOrder.id },
+          { headers: authHeader })
         this.setState({
           currentOrder: response.data.order,
           orderMessage: `${product.title} was added to your order`
@@ -46,7 +45,25 @@ class Menu extends React.Component {
     }
   }
 
+  confirmOrder = async () => {
+    let authHeader = JSON.parse(localStorage.getItem("credentials"))
+    try {
+      let response = await axios.put(
+        `/orders/${this.state.currentOrder.id}`,
+        { order_id: this.state.currentOrder.id, confirmed: true },
+        { headers: authHeader })
+      this.setState({
+        currentOrder: undefined,
+        orderMessage: response.data.message
+      })
+    }
+    catch (error) {
+
+    }
+  }
+
   render() {
+    const { orderMessage, currentOrder, authenticated } = this.state
     return (
       <Container className="page-container" fluid>
         <Grid textAlign="center" divided="vertically" >
@@ -60,17 +77,21 @@ class Menu extends React.Component {
           <Grid.Row textAlign="center">
             <Registration setAuthentication={() => this.setAuthentication()} />
           </Grid.Row>
-          {this.state.orderMessage && (
+          {orderMessage && (
             <div >
-              <p id="order-message">{this.state.orderMessage}</p>
-
-              <p id="order-length">You have {this.state.currentOrder.items.length} {this.state.currentOrder.items.length > 1 ? "items" : "item"} in your order</p>
-              <ItemList orderList={this.state.currentOrder.items} />
+              <p id="order-message">{orderMessage}</p>
+              {currentOrder && (
+                <div>
+                  <p id="order-length">You have {currentOrder.items.length} {currentOrder.items.length > 1 ? "items" : "item"} in your order</p>
+                  <ItemList orderList={currentOrder.items} />
+                  <Button data-cy="confirm-button" onClick={() => this.confirmOrder()} />
+                </div>
+              )}
             </div>
           )}
           <Grid.Row>
             <Item.Group>
-              <MenuList addToOrder={this.addToOrder} authenticated={this.state.authenticated} />
+              <MenuList addToOrder={this.addToOrder} authenticated={authenticated} />
             </Item.Group>
           </Grid.Row>
         </Grid>
